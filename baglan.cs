4e8e3a956a44555544dd234e86aff8bbfe043ab6
Emrullah AYDIN <61662213+emrullahaydin@users.noreply.github.com>
2021-01-17 17:51:15 +0300

    Update baglan.cs
    Şifrenizi kendiniz giriniz.

--- baglan.cs
+++ baglan.cs
@@ -10,7 +10,7 @@ namespace AracServisTakip
         public static SqlConnection vtbaglan()
         {
             if (baglanti == null)
-                baglanti = new SqlConnection(@"Data Source=BT\SQLSERVER;Initial Catalog=AracServisTakip;User ID=sa;Password=Emo_93351986");
+                baglanti = new SqlConnection(@"Data Source=BT\SQLSERVER;Initial Catalog=AracServisTakip;User ID=sa;Password=xxxx");
 
             if (baglanti.State == ConnectionState.Closed)
                 baglanti.Open();
